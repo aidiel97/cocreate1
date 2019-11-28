4a1e0fbd7912efe6ce5954e6b38af399c7681832
Cristoph Adam Sugianto <sugianto@btpn.com>
2019-11-28 09:53:32 +0700

    create request logger

--- src/server/index.js
+++ src/server/index.js
@@ -3,14 +3,21 @@ const app = express();
 const port = 3000;
 
 const router = require('../router');
-const { errorHelper, notFoundHandler } = require('../utils');
+const { 
+  errorHelper, 
+  notFoundHandler,
+  requestLogger,
+  logger
+} = require('../utils');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use(requestLogger);
 
 router(app);
 
 app.use(notFoundHandler);
 app.use(errorHelper);
 
-app.listen(port, () => console.log(`app listen on port ${port}`));
+app.listen(port, () => logger.info(`app listen on port ${port}`));
