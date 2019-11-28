b99ee97c6c9e09dacab328b8ede1a668734211e1
erickoyap <ericko.yaputro@btpn.com>
2019-11-28 09:07:56 +0700

    Add insert

--- src/handler/usersHandler/registerUserHandler.js
+++ src/handler/usersHandler/registerUserHandler.js
@@ -1,8 +1,20 @@
 const registerUserHandler = async (req, res) => {
   //LOGIC HERE
+  const { db } = req.app.locals;
+  const payload = req.body;
+  const collectionName = process.env.MONGO_DB_COLLECTION_USERS;
 
-  //END OF LOGIC
-  res.send(req.body);
+  try {
+    const result = await db.insertOne(collectionName, payload);
+    if (result) {
+      res.send(result);
+    }
+
+    throw new Error("Failed to insert!");
+  }
+  catch(err){
+    throw err;
+  }
 };
 
 module.exports = registerUserHandler;
